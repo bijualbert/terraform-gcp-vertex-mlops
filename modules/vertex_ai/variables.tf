4f602a08953ca025499cf552ed614566bffc797d
biju <bijualbert@gmail.com>
2026-02-08 12:58:34 +0530

    Updated vertext_ai/varibales.tf

--- modules/vertex_ai/variables.tf
+++ modules/vertex_ai/variables.tf
@@ -1,5 +1,9 @@
-{
-  owner = "bijualbert"
-  env = "dev"
-  cost_center = "mlops"
+variable "labels" {
+  description = "Common labels applied to all resources"
+  type        = map(string)
+  default = {
+    owner       = "bijualbert"
+    env         = "dev"
+    cost_center = "mlops"
+  }
 }
