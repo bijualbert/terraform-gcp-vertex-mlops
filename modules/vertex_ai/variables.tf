6415838b23349ab7086da126406696953ca1fec8
biju <bijualbert@gmail.com>
2026-02-08 12:50:05 +0530

    Removed vertex_ai/variables.tf contents to fix error

--- modules/vertex_ai/variables.tf
+++ modules/vertex_ai/variables.tf
@@ -1,7 +1,1 @@
-module "secure_bucket" {
-  source      = "../modules/gcs_bucket"
-  name        = var.bucket_name
-  location    = var.region
-  project_id  = var.project_id
-  labels      = var.labels
-}
+
